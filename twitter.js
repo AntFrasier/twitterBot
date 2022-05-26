1d61fff3c704aa95c5bcf8016f3c0bbfbe41255f
AntFrasier <frasier@hotmail.fr>
2022-05-26 17:46:55 +0200

    Remove .env files

--- twitter.js
+++ twitter.js
@@ -16,37 +16,30 @@ const myId = process.env.MY_ID
 
 // With default prefix
 const search = async() => {
-  // console.log(date);
-const results = await client.v2.get('tweets/search/recent', { query: 'Follow RT #giveaway -is:retweet', max_results: 10, 'tweet.fields': ['referenced_tweets'], expansions : ['author_id'] });
-const length = results.data.length;
+  const results = await client.v2.get('tweets/search/recent', { query: 'Follow RT #giveaway -is:retweet', max_results: 10, 'tweet.fields': ['referenced_tweets'], expansions : ['author_id'] });
+  const length = results.data.length;
+  console.log(length)
+  for(i=0 ; i<length ; i++) {
+  // end_time :'2022-05-25T00:00:00Z',
+    const users = await client.v2.tweetLikedBy(results.data[i].id)
+    console.log('********************************',users)
+    if (users.meta.result_count === 0 ) continue;
+    const usersLikedLength = await users.data.length;
+    var isLiked = false;
+    for (j=0 ; j<usersLikedLength ; j++) {
+      if (users.data[j].id === '1529460352844451843'){
+        isLiked = true;
+        continue;
+        }  else {
+             console.log( 'data that will be liked ', results.data[i]);  
+             const liked = await client.v2.like('1529460352844451843', results.data[i].id);
+             const retweet = await client.v2.retweet('1529460352844451843', results.data[i].id);
+             const reply = await client.v2.reply('Oo Hey @Theda32516554, @BolandDrummer, @Oleta50707472, @Emiko74547042, @Nelda29443713 have a look ! ', results.data[i].id )
+             const following = await client.v2.follow('1529460352844451843', results.data[i].author_id);
+            }
 
-console.log(length)
-for(i=0 ; i<length ; i++) {
-// end_time :'2022-05-25T00:00:00Z',
-
-      let users = await client.v2.tweetLikedBy(results.data[i].id)
-      console.log('********************************',users)
-      let usersLikedLength = users.data.length;
-      var isLiked = false;
-        for (j=0 ; j<usersLikedLength ; j++) {
-                  if (users.data[j]) {
-                    if (users.data[j].id === '1529460352844451843') isLiked = true
-                  } else isLiked = false
-                }
-
-
-      if (!isLiked)  console.log(results.data[i]);
-
-    // if (!isLiked) {
-    //       const liked = await client.v2.like('1529460352844451843', results.data[i].id);
-    //       const retweet = await client.v2.retweet('1529460352844451843', results.data[i].id);
-    //       const reply = await client.v2.reply('Oo Hey @Theda32516554, @BolandDrummer, @Oleta50707472, @Emiko74547042, @Nelda29443713 have a look ! ', results.data[i].id )
-    //       const following = await client.v2.follow('1529460352844451843', results.data[i].author_id);
-
-    // console.log(liked);
-  // }
-}
-
+      } 
+    }
 }
 
 search();
